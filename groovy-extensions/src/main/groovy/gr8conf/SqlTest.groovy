114fdac565df0fe925bcc4a0ebac706081891dd9
Daniel Woods <daniel.p.woods@gmail.com>
2014-07-25 19:04:06 -0700

    commit #2 -- fixed sql typechecked

--- groovy-extensions/src/main/groovy/gr8conf/SqlTest.groovy
+++ groovy-extensions/src/main/groovy/gr8conf/SqlTest.groovy
@@ -10,7 +10,7 @@ class SqlTest {
     def db = [url:'jdbc:hsqldb:mem:testDB', user:'sa', password:'', driver:'org.hsqldb.jdbc.JDBCDriver']
     def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
-    sql.eachRow('select * frm PROJECT') { row ->
+    sql.eachRow('select * from PROJECT') { row ->
       println row
     }
   }
