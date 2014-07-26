77cffd3a3d1b241b16cc3b71bd757d4c441b4fc3
Daniel Woods <daniel.p.woods@gmail.com>
2014-07-25 19:03:37 -0700

    commit #2 -- broken sql typechecked

---
+++ groovy-extensions/src/main/groovy/gr8conf/SqlTest.groovy
@@ -0,0 +1,17 @@
+package gr8conf
+
+import groovy.sql.Sql
+import groovy.transform.TypeChecked
+
+@TypeChecked(extensions = ['SqlTypeCheckingExt.groovy'])
+class SqlTest {
+
+  static void main(_) {
+    def db = [url:'jdbc:hsqldb:mem:testDB', user:'sa', password:'', driver:'org.hsqldb.jdbc.JDBCDriver']
+    def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+
+    sql.eachRow('select * frm PROJECT') { row ->
+      println row
+    }
+  }
+}
---
+++ groovy-extensions/src/main/resources/SqlTypeCheckingExt.groovy
@@ -0,0 +1,25 @@
+package gr8conf
+
+import net.sf.jsqlparser.parser.CCJSqlParserManager
+
+afterMethodCall { mc ->
+  def receiver = mc.receiver
+  if (!isVariableExpression(receiver)) return
+
+  def method = getTargetMethod(mc)
+
+  if (classNodeFor(groovy.sql.Sql) == getType(receiver) && method.name == 'eachRow') {
+    def argList = getArguments(mc)
+    if (argList && isConstantExpression(argList[0])) {
+      def pm = new CCJSqlParserManager();
+      def sqlQuery = argList[0].text
+
+      try {
+        pm.parse(new StringReader(sqlQuery))
+      } catch (e) {
+        addStaticTypeError("SQL query is not valid: " + e, argList[0])
+      }
+    }
+  }
+}
+
